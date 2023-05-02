3610978007a0d4d9a96718b670f9c36cfe4c24d6
Joeward Peralta <Joeward_peralta@outlook.com>
2023-05-02 16:56:57 -0400

    Added new route for all the navigation links

--- app.js
+++ app.js
@@ -1,12 +1,56 @@
+const path = require("path");
+
+// Setting up express
 const express = require("express");
 const app = express();
 const HTTPS_PORT = process.env.port || 3000;
-const path = require("path");
 
+// Setting up handlebars
+const exphbs = require("express-handlebars");
+app.engine(".hbs", exphbs.engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+// Using static files
 app.use(express.static("public"));
 
+// Navigate to homepage
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/index.html"));
+  res.render("index", { layout: "default" });
+});
+
+// Navigate to about page
+app.get("/about", (req, res) => {
+  res.render("about", { layout: "default" });
+});
+
+// Navigate to contact page
+app.get("/contact", (req, res) => {
+  res.render("contact", { layout: "default" });
+});
+
+// Navigate to portfolio page
+app.get("/portfolio", (req, res) => {
+  res.render("portfolio", { layout: "default" });
+});
+
+// Navigate to privacy page
+app.get("/privacy", (req, res) => {
+  res.render("privacy", { layout: "default" });
+});
+
+// Navigate to resume
+app.get("/resume", (req, res) => {
+  res.render("resume", { layout: "default" });
+});
+
+// Navigate to services page
+app.get("/services", (req, res) => {
+  res.render("services", { layout: "default" });
+});
+
+// Navigate to terms and condition page
+app.get("/terms", (req, res) => {
+  res.render("terms", { layout: "default" });
 });
 
 app.use((req, res) => {
