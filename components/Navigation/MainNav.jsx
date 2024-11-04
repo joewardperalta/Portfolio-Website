a9c5ff1dd09b9735ea53884f8a7651165507b1ea
Joe Peralta <jperalta11@myseneca.ca>
2024-11-04 09:50:22 -0500

    Make the navbar position fixed on top when the user scroll down the page

--- components/Navigation/MainNav.jsx
+++ components/Navigation/MainNav.jsx
@@ -1,23 +1,56 @@
 "use client";
 import Link from "next/link";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import Logo from "../Logo";
 
 export default function MainNav() {
   const navToggler = useRef(null);
+  const navbar = useRef(null);
+  const isFixed = useRef(false);
 
   function handleClick() {
     navToggler.current.classList.toggle("hidden");
   }
 
+  // if the user scroll down the page, make the navbar fixed on the top,
+  // otherwise make the navbar absolute
+  function handleNavbarPosition() {
+    if (window.scrollY > 0) {
+      navbar.current.classList.remove("absolute");
+      navbar.current.classList.add("fixed", "bg-black");
+    } else {
+      navbar.current.classList.remove("fixed", "bg-black");
+      navbar.current.classList.add("absolute");
+      isFixed.current = false;
+    }
+  }
+
+  useEffect(() => {
+    // determine the navbar's initial position
+    handleNavbarPosition();
+
+    // detect scroll down event, and dynamically change the navbar's
+    // position
+    document.addEventListener("scroll", () => {
+      handleNavbarPosition();
+    });
+  }, []);
+
   return (
-    <div className="flex justify-between w-full absolute px-6 py-8 tablet:px-10 laptop:px-16">
-      <div className="w-full mx-auto laptop:max-w-[1160px] desktop:max-w-[1320px]">
-        <div className="z-50 flex justify-between w-full">
+    <div
+      className="flex justify-between w-full absolute px-6 py-8 
+      tablet:px-10 laptop:px-16"
+      ref={navbar}
+    >
+      <div
+        className="w-full mx-auto flex justify-between items-center 
+        laptop:max-w-[1160px] desktop:max-w-[1320px]"
+      >
+        <div className="z-50 flex justify-between items-center w-full">
           <Logo />
 
           <button
-            className="flex flex-col py-1.5 gap-1.5"
+            className="flex flex-col py-1.5 gap-1.5 laptop:hidden"
             onClick={handleClick}
           >
             <div className="w-7 h-0.5 bg-white"></div>
@@ -26,9 +59,16 @@ export default function MainNav() {
           </button>
         </div>
 
-        <div className="hidden" ref={navToggler}>
-          <nav className="bg-black fixed left-0 top-0 w-full h-screen flex justify-center items-center">
-            <ul className="text-center space-y-10 text-white flex flex-col">
+        <div className="hidden laptop:block" ref={navToggler}>
+          <nav
+            className="bg-black fixed left-0 top-0 w-full 
+            h-screen flex justify-center items-center laptop:relative 
+            laptop:h-fit laptop:bg-transparent"
+          >
+            <ul
+              className="text-center text-white 
+              flex flex-col gap-10 laptop:flex-row"
+            >
               <Link href="#">Home</Link>
               <Link href="#about">About</Link>
               <Link href="#services">Services</Link>
